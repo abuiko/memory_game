5a627aa26e800f173e40b66727ca1bacc3a80eff
Alena Buiko <abuiko.ma@gmail.com>
2021-01-25 19:55:06 -0500

    match two same flipped cards / add score

--- app.js
+++ app.js
@@ -51,10 +51,14 @@ document.addEventListener("DOMContentLoaded", () => {
     ]
 
     cardArray.sort(() => 0.5 - Math.random());
+    console.log(cardArray);
 
     const grid = document.querySelector('.grid');
+    const resultDisplay = document.querySelector('#score');
+
     let cardsChosen = [];
     let cardsChosenIds = [];
+    let cardsWon = [];
 
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
@@ -67,17 +71,50 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    createBoard();
+
 
     function flipCard() {
         let cardId = this.getAttribute('data-id');
         cardsChosen.push(cardArray[cardId].name);
         cardsChosenIds.push(cardId);
         this.setAttribute('src', cardArray[cardId].img);
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500);
+        }
+    }
+
+    function checkForMatch() {
+        const cards = document.querySelectorAll("img");
+        const optionOneId = cardsChosenIds[0];
+        const optionTwoId = cardsChosenIds[1];
+        if (optionOneId === optionTwoId) {
+            alert('You have clicked the same image!');
+            cards[optionOneId].setAttribute('src', 'img/nature.png');
+            cards[optionTwoId].setAttribute('src', 'img/nature.png');
+        } else if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You have found a match!');
+            cards[optionOneId].removeEventListener('click', flipCard);
+            cards[optionTwoId].removeEventListener('click', flipCard);
+            cardsWon.push(cardsChosen);
+        } else {
+            cards[optionOneId].setAttribute('src', 'img/nature.png');
+            cards[optionTwoId].setAttribute('src', 'img/nature.png');
+            alert('Sorry, try again');
+        }
+        cardsChosen = [];
+        cardsChosenIds = [];
+        console.log(cardsChosenIds);
+        console.log(cardsChosen);
+        resultDisplay.textContent = cardsWon.length;
+        if (cardsWon.length === 6) {
+            resultDisplay.textContent = 'YOU HAVE WON!';
+        }
 
 
     }
 
+
+    createBoard();
 
 
 
